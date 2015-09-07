a8fff2703678a0eb76f4edfd80b318768a6fff02
Shawn Hall <shawnahall71@users.noreply.github.com>
2015-09-06 20:37:44 -0400

    Part two of working method for adding members to directory

--- diradm/index.php
+++ diradm/index.php
@@ -8,6 +8,10 @@
 		<script src="/js/ui-bootstrap-tpls-0.11.2.min.js"></script>
 		<script>
 			var site = "http://www.tworidges.org";
+			var readpage = "/diradm/read.php";
+			var createpage = "/diradm/create.php";
+			var updatepage = "/diradm/update.php";
+			var deletepage = "/diradm/delete.php";
 			angular.module('myApp', ['ui.bootstrap']);
 			angular.module('myApp').controller('pageCtrl', function ($scope,$http) {
 				change = function () {
@@ -17,13 +21,13 @@
 					if (!isNaN($scope.currentPage)) {
 						offset = ($scope.currentPage - 1) * limit;
 					}
-					$http.get(site + page + "?offset=" + offset + "&limit=" + limit).success(function(response) {
+					$http.get(site + readpage + "?offset=" + offset + "&limit=" + limit).success(function(response) {
 						console.log('offset is: ' + offset);
 						console.log('limit is: ' + limit);
 						$scope.names = response;
 					});
 
-					$http.get(site + page + "?q=count").success(function(response) {
+					$http.get(site + readpage + "?q=count").success(function(response) {
 						$scope.totalItems = response;
 					});
 				}
@@ -37,53 +41,100 @@
 					change();
 				};
 			});
-			angular.module('myApp').controller('addModalCtrl', function ($scope,$http,$modal) {
+			angular.module('myApp').controller('createModalCtrl', function ($scope,$http,$modal) {
 				$scope.entry = {
+			        FirstName: null,
+			        LastName: null,
+			        Address: null,
+			        City: null,
+			        State: null,
+			        Zip: null,
+			        HomePhone: null,
+			        CellPhone: null,
+			        WorkPhone: null,
+			        Email: null,
+			        Image: null,
 			    };
 
 				$scope.open = function() {
-					var addModalInstance = $modal.open({
-						templateUrl: "addModalContent.html",
-						controller: "addModalInstance",
+					var createModalInstance = $modal.open({
+						templateUrl: "createModalContent.html",
+						controller: "createModalInstance",
 						backdrop: 'static'
 					});
 				};
 			});
+			angular.module('myApp').controller('createModalInstance', function ($scope,$http,$modalInstance) {
 
-				  $modalInstance.close("test");
+				$scope.close = function () {
+					$http({
+						url: site + createpage,
+						method: "POST",
+						data: $scope.entry,
+						headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+					}).success(function(data, status, headers, config){});
+				  	$modalInstance.close('closing');
 				};
 
 				$scope.cancel = function () {
-				  $modalInstance.dismiss('cancel');
+				  	$modalInstance.dismiss('cancel');
 				};
 			});
 		</script>
-		<script type="text/ng-template" id="addModalContent.html">
+		<script type="text/ng-template" id="createModalContent.html">
 	        <div class="modal-header">
 	            <h3 class="modal-title">Add new entry to directory</h3>
 	        </div>
             <form ng-submit="submit()">
 				<div class="modal-body form-group">
 					<div class="form-group">
+						<label>First Name</label>
+						<input type="text" class="form-control" placeholder="Enter first name" ng-model="entry.FirstName" />
+					</div>
+					<div class="form-group">
+						<label>Last Name</label>
+						<input type="text" class="form-control" placeholder="Enter last name" ng-model="entry.LastName" />
 					</div>
 					<div class="form-group">
 						<label>Address</label>
+						<input type="text" class="form-control" placeholder="Enter address" ng-model="entry.Address" />
+					</div>
+					<div class="form-group">
+						<label>City</label>
+						<input type="text" class="form-control" placeholder="Enter city" ng-model="entry.City" />
+					</div>
+					<div class="form-group">
+						<label>State</label>
+						<input type="text" class="form-control" placeholder="Enter state" ng-model="entry.State" />
+					</div>
+					<div class="form-group">
+						<label>Zip</label>
+						<input type="text" class="form-control" placeholder="Enter zip" ng-model="entry.Zip" />
 					</div>
 					<div class="form-group">
 						<label>Home Phone</label>
+						<input type="text" class="form-control" placeholder="Enter home phone" ng-model="entry.HomePhone" />
 					</div>
 					<div class="form-group">
 						<label>Cell Phone</label>
+						<input type="text" class="form-control" placeholder="Enter cell phone" ng-model="entry.CellPhone" />
 					</div>
 					<div class="form-group">
 						<label>Work Phone</label>
+						<input type="text" class="form-control" placeholder="Enter work phone" ng-model="entry.WorkPhone" />
 					</div>
 					<div class="form-group">
 						<label>Email</label>
+						<input type="text" class="form-control" placeholder="Enter email" ng-model="entry.Email" />
+					</div>
+					<div class="form-group">
+						<label>Image</label>
+						<input type="text" class="form-control" placeholder="Enter image" ng-model="entry.Image" />
 					</div>
 				</div>
 				<div class="modal-footer">
 				    <button class="btn btn-warning" ng-click="cancel()">Cancel</button>
+				    <input type="submit" class="btn primary-btn" value="Submit" ng-click="close()" data-dismiss="modal"/>
 				</div>
 	        </form>
 	    </script>
@@ -95,7 +146,7 @@
     <!-- Begin page content -->
 	<div class="container mytext" ng-controller="pageCtrl">
 		<div class="row">
-			<div ng-controller="addModalCtrl">
+			<div ng-controller="createModalCtrl">
 				<div class="col-lg-4 col-sm-6 col-xs-12">
 					<div class="text-center padtop">
 						<button class="btn btn-success" ng-click="open()">
